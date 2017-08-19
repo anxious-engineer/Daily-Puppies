8a1892b8786b94f8b1dfcc607eb0fc8b285b85d2
David Parsons <david.parsons@rutgers.edu>
2017-08-19 16:38:04 -0400

    Static Page Creation

--- scrape.py
+++ scrape.py
@@ -7,6 +7,12 @@ subreddits = [
     'aww'
 ]
 
+start_doc = '<!DOCTYPE html>\n<html>\n<title>Puppies</title>\n<head>\n</head>\n<body>\n<div id=\'photos\'>\n'
+
+tags = ''
+
+end_doc = '</div>\n</body>\n<footer>\n<p>Developed by: <a href="../">David Parsons</a>\n</br>\n<a href="http://github.com/Parsons-David/Puppy-Scraper">Source</a>\n</p>\n</footer>\n</html>'
+
 def get_posts(subreddit, limit=None):
     url = 'https://www.reddit.com/r/%s/top/.json' % subreddit
 
@@ -17,7 +23,8 @@ def get_posts(subreddit, limit=None):
 
     data = json.loads(urllib.urlopen(url).read())
 
-    print(data.keys())
+    if('error' in data.keys()):
+        return {}
 
     # print(data)
 
@@ -26,22 +33,33 @@ def get_posts(subreddit, limit=None):
 
     return posts
 
-def save_picture(url):
-    f = open('imgs/' + url.split('/')[-1],'wb')
+def save_image(url):
+    global tags
+    f = open('docs/imgs/' + url.split('/')[-1],'wb')
+    tags += '<img src=\'%s\'' % ('imgs/' + url.split('/')[-1])
+    tags += ' height="500">\n</br>\n'
     f.write(urllib.urlopen(url).read())
     f.close()
+
+# def send_image(url):
 
 if __name__=='__main__':
 
     for sub in subreddits:
 
-        sub_posts = get_posts(sub, 4)
+        sub_posts = get_posts(sub, 10)
 
         for post in sub_posts:
             image_url = post['data']['url'].encode('utf-8')
             print('Opening... %s' % image_url)
 
             if image_url.endswith('.jpg') or image_url.endswith('.gif'):
-                save_picture(image_url)
+                save_image(image_url)
 
-            # webbrowser.open(image_url)
+
+            # else:
+            #     webbrowser.open(image_url)
+
+    f = open('docs/index.html', 'wb')
+    f.write(start_doc + tags + end_doc)
+    f.close()
